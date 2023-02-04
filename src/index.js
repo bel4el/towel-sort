30d3d0f6c368cbc0275b4a4e3d8e875118d4d954
Mikhail Naryvonchyk <bel4elz@gmail.com>
2023-02-04 23:13:26 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ 
+  if (matrix == "" || matrix == [] || matrix == undefined) return [];
+
+  if (matrix.length < 4) {
+    return matrix.splice(0, 3, matrix[1].reverse()).flat();
+  } else {
+    return matrix.splice(0, 4, matrix[1].reverse(), matrix[3].reverse() ).flat();
+  }
 }
